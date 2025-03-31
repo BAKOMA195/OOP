3da2e312130a9d283206c03f8b3c604c3a04fda0
BAKOMA195 <ylitochka195@gmail.com>
2025-03-31 15:52:53 +0500

    Update OOP3part1_Form1.cs

--- OOP3part1_Form1.cs
+++ OOP3part1_Form1.cs
@@ -1,27 +1,22 @@
+при зажатии ctrl по клику левой кнопкой должны выделятся сразу несколько кругов, только я могу выделить 2, 3 и т,д в моем коде почему я не могу выделить больше 1 объекта
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static OOP3part1.Form1;
 
 namespace OOP3part1
 {
-
     public partial class Form1 : Form
     {
-
         private List<CCircle> circles = new List<CCircle>(); // Список кругов
 
         public Form1()
         {
             InitializeComponent();
-            this.MouseClick += Form1_MouseClick; // Подписываемся на клик мыши
-            this.Paint += Form1_Paint; // Подписываемся на перерисовку
+            this.MouseClick += Form1_MouseClick; // Обрабатываем клики мыши
+            this.Paint += Form1_Paint; // Перерисовка формы
+            this.KeyDown += Form1_KeyDown; // Обработка клавиш
+            this.KeyPreview = true; // Позволяет форме перехватывать клавиши
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -34,21 +29,71 @@ namespace OOP3part1
             public int X;
             public int Y;
             public int R = 20;
+            public bool IsSelected = false; // Флаг выделения
+
             public CCircle(int x, int y)
             {
-                X = x; Y = y;
+                X = x;
+                Y = y;
             }
 
+            // Проверка, находится ли точка внутри круга
+            public bool ContainsPoint(int x, int y)
+            {
+                int dx = x - X;
+                int dy = y - Y;
+                return dx * dx + dy * dy <= R * R;
+            }
+
+            // Отрисовка круга (красный, если выделен)
             public void Draw(Graphics g)
             {
-                g.FillEllipse(Brushes.Magenta, X - R, Y - R, R * 2, R * 2);
+                Brush brush = IsSelected ? Brushes.Red : Brushes.Magenta;
+                g.FillEllipse(brush, X - R, Y - R, R * 2, R * 2);
             }
-
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
-            circles.Add(new CCircle(e.X, e.Y)); // Добавляем круг по координатам клика
+            if (e.Button == MouseButtons.Right) // ПКМ - создаем новый круг
+            {
+                circles.Add(new CCircle(e.X, e.Y));
+            }
+            else if (e.Button == MouseButtons.Left) // ЛКМ - выделение
+            {
+                bool found = false; // Флаг, был ли клик по кругу
+                bool ctrlPressed = ModifierKeys.HasFlag(Keys.Control); // Проверяем, зажат ли Ctrl
+
+                foreach (var circle in circles)
+                {
+                    if (circle.ContainsPoint(e.X, e.Y))
+                    {
+                        found = true;
+                        if (ctrlPressed)
+                        {
+                            circle.IsSelected = !circle.IsSelected; // Переключаем выделение
+                        }
+                        else
+                        {
+                            circle.IsSelected = true; // Обычное выделение
+                        }
+                    }
+                    else if (!ctrlPressed) // Если Ctrl не зажат - снимаем выделение с остальных
+                    {
+                        circle.IsSelected = false;
+                    }
+                }
+
+                // Если кликнули на пустое место без Ctrl, снимаем выделение со всех
+                if (!found && !ctrlPressed)
+                {
+                    foreach (var circle in circles)
+                    {
+                        circle.IsSelected = false;
+                    }
+                }
+            }
+
             this.Invalidate(); // Перерисовываем форму
         }
 
@@ -56,7 +101,16 @@ namespace OOP3part1
         {
             foreach (var circle in circles)
             {
-                circle.Draw(e.Graphics); // Рисуем все круги из списка
+                circle.Draw(e.Graphics);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete) // Удаление выделенных кругов
+            {
+                circles.RemoveAll(c => c.IsSelected);
+                this.Invalidate();
             }
         }
     }
